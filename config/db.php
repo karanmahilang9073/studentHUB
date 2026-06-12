056341a20af268df8847f0f5f42ac5df96897beb
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-06-12 13:57:07 +0530

    credential hidden

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
+require_once 'config.php';
 $conn = mysqli_connect(
-    "localhost",
-    "root",
-    "km907316",
-    "studenthub"
+    DB_HOST,
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
 );
 if(!$conn){
     die('connection failed:'. mysqli_connect_error());
